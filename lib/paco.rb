2ca1c9b8f664f6b4c8ccad8a37070578d23fe05c
takashi nagayasu <regist@g-onion.org>
2015-01-04 11:53:33 +0900

    Add Pacofile feature

--- lib/paco.rb
+++ lib/paco.rb
@@ -4,5 +4,26 @@ require "paco/repository"
 require "paco/env"
 
 module Paco
-  # Your code goes here...
+  class Config
+    include Singleton
+
+    attr_accessor :repos, :env, :dependencies
+  end
+
+  def env(envname)
+    Config.instance.env = Paco::Env::Unity.new
+  end
+
+  def paco(*args)
+    Config.instance.dependencies = [] unless Config.instance.dependencies
+    Config.instance.dependencies.push args
+  end
+
+  def source(*args)
+    repos = args.shift
+    Config.instance.repos = \
+      Repository::GoogleDrive.new(repos[:email], repos[:pem], repos[:collection_url])
+  end
+
+  module_function :paco, :source, :env
 end
